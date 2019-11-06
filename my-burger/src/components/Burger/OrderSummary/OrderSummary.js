ba5b772d98a57e2b507c2b46376440f242a67acb
samareshmandal <57229191+samareshmandal@users.noreply.github.com>
2019-11-06 14:42:36 +0530

    Implemented button and updated order summary

--- my-burger/src/components/Burger/OrderSummary/OrderSummary.js
+++ my-burger/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = (props) => {
 	const ingredientsSummary = Object.keys(props.ingredients).map((key) => {
@@ -16,7 +17,16 @@ const orderSummary = (props) => {
 			<h3>Your Order</h3>
 			<p>A delicius burger with the following ingredients:</p>
 			<ul>{ingredientsSummary}</ul>
+			<p>
+				<strong>Total Price: {props.price.toFixed(2)}</strong>
+			</p>
 			<p>Continue to checkout?</p>
+			<Button btnType="Danger" clicked={props.purchaseCancelled}>
+				Cancel
+			</Button>
+			<Button btnType="Success" clicked={props.purchaseContinued}>
+				Continue
+			</Button>
 		</Aux>
 	);
 };
--- my-burger/src/containers/BurgerBuilder/BurgerBulder.js
+++ my-burger/src/containers/BurgerBuilder/BurgerBulder.js
@@ -74,6 +74,14 @@ class BurgerBuilder extends Component {
 		this.setState({ purchasing: true });
 	};
 
+	purchaseCancelHandler = () => {
+		this.setState({ purchasing: false });
+	};
+
+	purchaseContinueHandler = () => {
+		alert("You continue..");
+	};
+
 	render() {
 		const disableInfo = {
 			...this.state.ingredients
@@ -83,8 +91,16 @@ class BurgerBuilder extends Component {
 		}
 		return (
 			<Aux>
-				<Modal show={this.state.purchasing}>
-					<OrderSummary ingredients={this.state.ingredients}></OrderSummary>
+				<Modal
+					show={this.state.purchasing}
+					modalClosed={this.purchaseCancelHandler}
+				>
+					<OrderSummary
+						ingredients={this.state.ingredients}
+						purchaseCancelled={this.purchaseCancelHandler}
+						purchaseContinued={this.purchaseContinueHandler}
+						price={this.state.totalPrice}
+					></OrderSummary>
 				</Modal>
 				<Burger ingredients={this.state.ingredients}></Burger>
 				<BuildControls
